fed813a66b078fd56451f1839471d7706c9a8e51
angleman <angleman@cormea.com>
2014-03-20 12:21:06 -0500

    initial

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # config4u
 
-environment, command line or json file with comments and single quotes config 
+Environment, command line or JSON file with comments and single quotes
+
+Wanted multiple tiered config like [config]() https://github.com/lorenwest/node-config will use ```NODE_ENV``` and [ALCE](https://github.com/walmartlabs/ALCE) parsing like [getconfig](https://github.com/HenrikJoreteg/getconfig) with the added ability to define ```config.json``` directory or filename via command line ```--config``` or environment variable ```NODE_CONFIG_DIR```
 
 ## Install :hammer:
 
@@ -11,8 +13,29 @@ npm install config4u
 ## Usage :bulb:
 
 ```js
-config4u = new require('config4u')
+{
+    postgres: {
+        "url": '...', // database connection url
+    }
+}
 ```
+
+```js
+config = new require('config4u')({ default: "values"})
+dburl  = config.postgres.url
+```
+
+```sh
+node myapp.js --config dir_or_filename // /somepath for /somepath.config.json or dev_config.json for /current_dir/dev_config.json
+```
+
+## Assignment order
+
+- Passed in defaults
+- ```config.json``` file (in current application directory if not defined by ```--config```)
+- ```config.json``` file (as defined by ```--config```)
+- ```config.json``` file (optionally if defined by ```NODE_CONFIG_DIR```)
+- ```NODE_ENV``` environment value as JSON string, ex: ```NODE_ENV = "{postgres: {url: '...'}}"```
 
 ## Contributions :muscle:
 
--- index.js
+++ index.js
@@ -1,16 +1,54 @@
 // config4u by angleman, MIT
-// environment, command line or json file with comments and single quotes config
+// Environment, command line or JSON file with comments and single quotes
 
-function Config4u(config) {
+function Config4u(DEFAULT) {
     "use strict"
-	
-	var API  = {
-		option: function() {
-		},
-		option2: function() {
+
+	var ALCE              = require('alce')                            // walmartlabs/ALCE
+	var xtend             = require('xtend')                           // Raynos/xtend
+	var fs                = require('fs')
+	var argv              = require('minimist')(process.argv.slice(2)) // substack/minimist
+	var configDir         = process.cwd()
+	var configFile        = 'config.json'
+	var configSource      = configDir + '/' + configFile
+	var config            = (DEFAULT) ? DEFAULT : {}
+	var json              = ''
+	var parsed            = {}
+
+	function findConfig(configSource) {
+		configSource      = argv.config
+		var asDir         = argv.config + '/' + configFile
+		var asFile        = configDir   + '/' + argv.config
+		if (configSource.substr(0,1) == '/' && fs.existsSync(configSource)) {
+			// fully qualified path that exists so it's good to go
+		} else if (fs.existsSync(asDir)) {
+			configSource  = asDir
+		} else if (fs.existsSync(asFile)) {
+			configSource  = asFile
 		}
+		var stat          = fs.statSync(configSource)
+		if (stat.isDirectory()) {
+			configSource += '/' + configFile
+		}
+		return configSource
 	}
-	return API
+	
+	function updateConfig(json, configSource) {
+		if (configSource) {
+			configSource  = findConfig(configSource)
+			json          = fs.readFileSync(configSource, 'utf8')
+		}
+		parsed            = ALCE.parse(json, {meta: false})
+		config            = xtend(config, parsed)
+	}
+	
+	// config file assigned via command line --config and/or via NODE_CONFIG_DIR environment variable
+	if (argv.config)                 updateConfig('', argv.config)
+	else                             updateConfig('', configSource)
+	if (process.env.NODE_CONFIG_DIR) updateConfig('', process.env.NODE_CONFIG_DIR)
+	if (process.env.NODE_ENV)        updateConfig(process.env.NODE_ENV)
+	
+	return config
 }
 
-module.exports = Config4u()
+module.exports = Config4u
